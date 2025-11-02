fcd7e8d8b8f7ebd5b78cdaa5e4e663ac7aa20d5f
Thxnxt <141336398+Thxnxt@users.noreply.github.com>
2025-11-02 18:53:16 +0700

    update connect website to mongoDB

--- web/app.py
+++ web/app.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, HTTPException, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://mongo:27017/")
+db = client["netconfig_db"]
+routers_collection = db["routers"]
 
 title = "NetConfig - "
 
@@ -18,9 +23,23 @@ async def home(request: Request):
 async def router_detail(request: Request):
     return templates.TemplateResponse("router_detail.html", {"title": title + "Detail", "request" : request})
 
-@app.get("/add")
-async def add_router(requset: Request):
-    pass
+@app.post("/add")
+async def add_router(
+    ip_address: str = Form(...),
+    username: str = Form(...),
+    password: str = Form(...),
+):
+    try:
+        router = {
+            "ip_address": ip_address,
+            "username": username,
+            "password": password,
+        }
+        routers_collection.insert_one(router)
+        return RedirectResponse(url="/", status_code=303)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
